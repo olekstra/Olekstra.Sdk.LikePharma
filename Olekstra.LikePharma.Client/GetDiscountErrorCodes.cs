2285b569a9125e63c16efd04581fef21690aa58f
Dmitry Popov <dmitry@parsec-club.ru>
2020-06-05 17:07:44 +0300

    Client v2.1.1: enum -> static class (GetDiscountErrorCodes)

--- Olekstra.LikePharma.Client/GetDiscountErrorCodes.cs
+++ Olekstra.LikePharma.Client/GetDiscountErrorCodes.cs
@@ -3,27 +3,27 @@
     /// <summary>
     /// Предопределённые коды ошибок (error_code) для <see cref="GetDiscountResponse"/>.
     /// </summary>
-    public enum GetDiscountErrorCode
+    public static class GetDiscountErrorCodes
     {
         /// <summary>
         /// Карта с указанным номером не найдена.
         /// </summary>
-        CardNotFound = 1,
+        public const int CardNotFound = 1;
 
         /// <summary>
         /// Карта с указанным найдена, но неактивна (не зарегистрирована или заблокирована).
         /// </summary>
-        CardDisabled = 101,
+        public const int CardDisabled = 101;
 
         /// <summary>
         /// Купон (одноразовый) уже был использован (погашен).
         /// </summary>
-        CouponAlreadyUsed = 4,
+        public const int CouponAlreadyUsed = 4;
 
         /// <summary>
         /// Телефон не найден (среди зарегистрированных телефонов).
         /// </summary>
-        PhoneNotFound = 5,
+        public const int PhoneNotFound = 5;
 
         ////NEED_SMS_CONFIRM = 2, // требуется смс подтверждение
         ////CARD_NOT_LINK_PHONE_NUMBER = 3, // телефон не привязан к карте
